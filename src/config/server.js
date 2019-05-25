713faa5cb1931d2003d0fba322a9e8bbe371b583
Minh Thong <thongnmtran@kms-technology.com>
2019-05-25 08:46:51 +0700

    connect to cloud

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicFolder: 'dist',
   port: process.env.PORT || 4000,
-  cloudAddress: 'http://localhost:5000'
+  cloudAddress: 'https://cloud-smart-garden.herokuapp.com' || 'http://localhost:5000'
 };
